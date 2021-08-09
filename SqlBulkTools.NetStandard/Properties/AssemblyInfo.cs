a80e737056e122e20b6debb9f5439da998caddbe
Brian Douglas <brian.douglas@bdcsoft.com>
2021-08-09 06:37:09 +0000

    update ver

--- SqlBulkTools.NetStandard/Properties/AssemblyInfo.cs
+++ SqlBulkTools.NetStandard/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SqlBulkTools")]
-[assembly: AssemblyDescription("https://github.com/ersensurekler/SqlBulkTools")]
+[assembly: AssemblyTitle("B3.SqlBulkTools")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SqlBulkTools")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyProduct("B3.SqlBulkTools")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
 [assembly:InternalsVisibleTo("SqlBulkTools.NetStandard.UnitTests")]
